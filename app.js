238bee8eae6f26b1bbb8d2ad8e103ed5d3eda40c
Thomas Schaaf <schaaf@komola.de>
2017-10-06 11:29:03 +0200

    First running demo

---
+++ app.js
@@ -0,0 +1,17 @@
+const http = require('http');
+const port = process.env.PORT || 3000;
+
+const requestHandler = (request, response) => {
+  console.log(request.url);
+  response.end(JSON.stringify(process.env));
+};
+
+const server = http.createServer(requestHandler);
+
+server.listen(port, (err) => {
+  if (err) {
+    return console.log('something bad happened', err);
+  }
+
+  console.log(`server is listening on ${port}`);
+});
